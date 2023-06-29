6d92d03fd40c20d2ad2f487c97e91b2409939013
B Gowri Sankar <sankaryadavrgukt@hotmail.com>
2023-06-29 09:04:21 +0530

    parsethecode in api

--- controllers/apiData.js
+++ controllers/apiData.js
@@ -2,9 +2,14 @@ exports.emailData = async (req, res) => {
 	try {
 		console.warn(req)
 		console.warn(req.body)
+		let msq = req.body;
+		let value = msg.match(/(^|[^\d])(\d{6})([^\d]|$)/);
+		if(value !== null){
+			console.log(value);
+		}
 		// console.log(JSON.stringify(req))
 		return res.status(200).json({
-			data: req.body,
+			data: value,
 		});
 	} catch (error) {
 		console.error(error);
