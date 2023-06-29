438268ef4acbcb89276ea101fb098078306d9f57
B Gowri Sankar <sankaryadavrgukt@hotmail.com>
2023-06-29 08:58:08 +0530

    removed lokijs code and added parser for plaint/text

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-var loki = require('lokijs');
-var db = new loki('db.json');
-var doctors = db.addCollection('doctors')
-doctors.insert({ name: `David Tennant`, doctorNumber: 10 });
-doctors.insert({ name: `Matt Smith`, doctorNumber: 11 });
-var res = doctors.find()
-console.log(res)
+
 const app = express();
 
 app.use(
@@ -15,8 +9,9 @@ app.use(
     origin: "*",
   })
 );
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.text({ type: 'text/plain' }))
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 const PORT = 8000;
 
