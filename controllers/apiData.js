83eb102a245eda27339d87ba9c0ceda3fdbc9463
B Gowri Sankar <sankaryadavrgukt@hotmail.com>
2023-06-30 08:55:44 +0530

    logic to get all codes

--- controllers/apiData.js
+++ controllers/apiData.js
@@ -1,12 +1,28 @@
+var loki = require('lokijs');
+var db = new loki('db.json');
+var codes = db.addCollection("codes")
+
+exports.getAllCodes = async (req, res) => {
+	try {
+		res.status(200).json({
+			data: codes.find().reverse()
+		})
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json(error.message);
+	}
+}
+
 exports.emailData = async (req, res) => {
 	try {
-		console.warn(req)
 		console.warn(req.body)
 		let msg = req.body;
 		let value = msg.match(/(^|[^\d])(\d{6})([^\d]|$)/);
 		if(value !== null){
 			console.log(value);
+			codes.insert({code: value[2]})
 		}
+		db.saveDatabase()
 		// console.log(JSON.stringify(req))
 		return res.status(200).json({
 			data: value,
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { emailData } = require("../controllers/apiData");
+const { emailData, getAllCodes } = require("../controllers/apiData");
 var loki = require('lokijs');
 var db = new loki('db.json');
 var doctors = db.addCollection('doctors')
@@ -7,8 +7,10 @@ var doctors = db.addCollection('doctors')
 //GET /createGmails
 
 router.post("/emailData", emailData)
+router.post("/getallcodes", getAllCodes)
 router.post("/adddata", (req, res) => {
     doctors.insert({ name: `Matt Smith`, doctorNumber: 11, created: new Date() });
+    db.saveDatabase();
     res.status(200).json({
         message:"Added data successfully"
     })
