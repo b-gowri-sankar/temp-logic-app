3f7a7551c394163b32e5d73534444e63d165928f
B Gowri Sankar <sankaryadavrgukt@hotmail.com>
2023-07-27 15:41:20 +0530

    support for multi/formdata

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+var multer = require("multer");
+var upload = multer();
+
 const app = express();
 
 app.use(
-  cors({
-    origin: "*",
-  })
+	cors({
+		origin: "*",
+	})
 );
-app.use(bodyParser.text({ type: 'text/plain' }))
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.text({ type: "text/plain" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(upload.array());
+app.use(express.static("public"));
 
 const PORT = 8000;
 
 //available routes
 app.use("/api", require("./routes"));
 
-
 app.listen(PORT, () => {
-  console.log(`Backend App Connected ${PORT}`);
+	console.log(`Backend App Connected ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,29 @@
 const router = require("express").Router();
 const { emailData, getAllCodes } = require("../controllers/apiData");
-var loki = require('lokijs');
-var db = new loki('db.json');
-var doctors = db.addCollection('doctors')
+var loki = require("lokijs");
+var db = new loki("db.json");
+var doctors = db.addCollection("doctors");
 //@desc Create Gmails
 //GET /createGmails
 
-router.post("/emailData", emailData)
-router.post("/getallcodes", getAllCodes)
+router.post("/emailData", emailData);
+router.post("/getallcodes", getAllCodes);
 router.post("/adddata", (req, res) => {
-    doctors.insert({ name: `Matt Smith`, doctorNumber: 11, created: new Date() });
-    db.saveDatabase();
-    res.status(200).json({
-        message:"Added data successfully"
-    })
-})
+	doctors.insert({ name: `Matt Smith`, doctorNumber: 11, created: new Date() });
+	db.saveDatabase();
+	res.status(200).json({
+		message: "Added data successfully",
+	});
+});
 
-router.post("/getalldata", (req,res) => {
-    res.status(200).json({
-        data: doctors.find().reverse()
-    })
-})
+router.post("/getalldata", (req, res) => {
+	res.status(200).json({
+		data: doctors.find().reverse(),
+	});
+});
 
 router.get("/fileupload", (req, res) => {
-    res.send(`<!DOCTYPE html>
+	res.send(`<!DOCTYPE html>
     <html>
     <body>
     
@@ -36,7 +36,12 @@ router.get("/fileupload", (req, res) => {
     
     </body>
     </html>
-    `)
-})
+    `);
+});
+
+router.post("/post-formdata", (req, res) => {
+	console.log(req.body.latitude);
+	return res.status(200).json("data is sent successfully");
+});
 
 module.exports = router;
